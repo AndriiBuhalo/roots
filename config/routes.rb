0c94975d99961d3ae7b798ddeeb71ee824188969
Ostap <semenchuk.ostap@icloud.com>
2021-11-07 22:53:42 +0200

    Added views for button

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   root :to => "welcome_pages#home"
   
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
 
 end
