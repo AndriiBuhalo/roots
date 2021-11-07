fcb2fda63f446101aa9ea82ec89904fde7751c5c
Max Stankovskyi <stankovskijm@gmail.com>
2021-11-07 15:33:23 +0200

    Added gem shoulda-matchers and validation tests

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'faker'
   gem 'pry-rails'
   gem 'rspec-rails', '~> 5.0.0'
+  gem 'shoulda-matchers', '~> 5.0'
 end
 
 group :development do
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
+
   factory :post, class: Post do
-    title { Faker::Lorem.title }
-    content { Faker::Lorem.characters }
+    title { Faker::Lorem.characters(number: 40) }
+    content { Faker::Lorem.characters(number: 65) }
   end
+
+
 end
