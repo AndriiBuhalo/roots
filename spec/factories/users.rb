32d626f057b44133d081f05132e99d1cc8f674b8
Ostap <semenchuk.ostap@icloud.com>
2021-11-07 15:11:38 +0200

    RSpec test for validation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    username {Faker::Name.username}
     email {Faker::Internet.email}
     password {Faker::Internet.password(min_length: 6)}
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "validation the name and makes sure it's not empty" do
+    user = User.new(username: '')
+    user.valid?
+    user.errors[:username].should_not be_empty
+  end
+
 end
