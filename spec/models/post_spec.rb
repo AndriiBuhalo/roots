1c78918fd512caca87799e8d5a0bfe59ee4fee69
Max Stankovskyi <stankovskijm@gmail.com>
2021-11-07 11:13:46 +0200

    Adding tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   context 'validation tests' do
-    let(:post) { build(:post) } #Post.new
+    # let(:post) { build(:post) } #Post.new
 
     it 'ensures title presence' do
-      post.title = nil
-      expect(post.save).to eq(false)
+      post = Post.new(content: "12345678910123456789101234567891012345678910123456789101234567891012345678910").save
+      # post.title = nil
+      expect(post).to eq(false)
     end
   end
 
