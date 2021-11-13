464112bd20091d90baded86835206c322c22240a
= <fergo.rossii@gmail.com>
2021-11-13 16:33:04 +0200

    Fixed attachment index_path and new_path

--- app/controllers/attachment_controller.rb
+++ app/controllers/attachment_controller.rb
@@ -2,54 +2,48 @@ class AttachmentController < ApplicationController
   before_action :set_post, only: [ :show, :edit, :update, :destroy ]
 
   def index
-    @image = Image.all.reverse
+    @attachments = Attachment.all.reverse
     @albums = Album.all
   end
 
   def findByTag
     @albums = Album.all
-    @image = Image.all.reverse
+    @attachments = Attachment.all.reverse
     @findIdByTag = []
     @findFilenameByTag = []
     ImageInfo.where(:keywords => params[:tag]).all.each do |findIdByTag|
       @findIdByTag << findIdByTag.image_id
       @findFilenameByTag << findIdByTag.filename
     end
-
   end
 
   def show
-    @image.attachments.each_with_index do |attachment, index|
-      if params[:index].to_i == index
-        @attachment = attachment
-      end
-    end
-    unless ImageInfo.exists?(filename: @attachment.identifier)
-       ImageInfo.create(filename: @attachment.identifier, image_id: @image.id)
-    end
     @albums = Album.all
     @imageInfo = ImageInfo.find_by_filename(@attachment.identifier)
   end
 
   def new
-    @image = Image.new
+    @attachment = Attachment.new
   end
 
   def create
-    if @image = Image.create(image_params)
-      if params[:image][:id].present?
-        @album = Album.find(params[:image][:id])
-        @album.images << @image
+    if @attachment = Attachment.create(file_params)
+      if params[:attachment][:id].present?
+        @album = Album.find(params[:attachment][:id])
+        @album.attachments << @attachment
       end
-      redirect_to image_index_path, success: t('image.controller.create')
+      unless ImageInfo.exists?(attachment_id: @attachment.id)
+         ImageInfo.create(attachment_id: @attachment.id)
+      end
+      redirect_to attachment_index_path, success: t('image.controller.create')
     else
-      redirect_to new_image_path, danger: t('image.controller.create error')
+      redirect_to new_attachment_path, danger: t('image.controller.create error')
     end
   end
 
   def update
-    if @image.update(image_params)
-      redirect_to image_index_path, success: t('image.controller.update')
+    if @attachment.update(file_params)
+      redirect_to attachment_index_path, success: t('image.controller.update')
     else
       flash.now[:danger] = t('image.controller.update error')
       render :edit
@@ -57,15 +51,15 @@ class AttachmentController < ApplicationController
   end
 
   def destroy
-    @image.destroy
-    redirect_to image_index_path, success: t('image.controller.destroy')
+    @attachment.destroy
+    redirect_to attachment_index_path, success: t('image.controller.destroy')
   end
 
   private def set_post
-    @image = Image.find(params[:id])
+    @attachment = Attachment.find(params[:id])
   end
 
-  def image_params
-    params.require(:image).permit(attachments: [])
+  def file_params
+    params.require(:attachment).permit(:file)
   end
 end
