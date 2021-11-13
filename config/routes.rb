114721e7389825f9c9bc3c89833feb34b5705578
= <fergo.rossii@gmail.com>
2021-11-13 15:26:27 +0200

    Added views for image_info and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   get 'welcome_pages/contacts'
 
   root :to => "welcome_pages#home"
+
+  resources :album
+  resources :attachment
+  resources :image_info
+  get 'findByTag' => 'image#findByTag'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
