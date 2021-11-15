87ceb068ed6988adabfbef8124c7f3254e68d55a
Nikolay010 <timofeev7300@ukr.net>
2021-11-15 16:05:31 +0200

    2222

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'welcome_pages/about'
   get 'welcome_pages/contacts'
 
-  root :to => "welcome_pages#home"  
+  root :to => "welcome_pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
