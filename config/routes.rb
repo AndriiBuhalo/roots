1f842ffcd68bdb3ac4be8f4e17a99cbff1e5ad65
Nikolay010 <timofeev7300@ukr.net>
2021-11-15 15:15:46 +0200

    fix database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'welcome_pages/contacts'
 
   root :to => "welcome_pages#home"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
