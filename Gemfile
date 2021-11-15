5b698c148ee45b32d6d315bc500622a9c8149b4e
= <fergo.rossii@gmail.com>
2021-11-15 18:18:21 +0200

    Added simple form and fixed styles

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'slim'
 gem 'devise'
 gem 'carrierwave', '~> 2.0'
 gem "mini_magick"
+gem 'simple_form'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/channels/scriptForImageFiles_channel.js
+++ app/javascript/channels/scriptForImageFiles_channel.js
@@ -1,34 +1,5 @@
 $( document ).ready(function() {
 
-$(document).on('change', '#file', function (e) {
-  $('.ImgFile').attr("src", window.URL.createObjectURL(this.files[0]));
-  $('.ImgFile').show();
-  $('.borderImgFile .title').hide();
-});
-
-$(document).on('change', '.attachments', function (e) {
-    var file, fileId, removeLink;
-
-    for (var i = 0, length = e.target.files.length; i < length; i++) {
-        fileId = i;
-        file = e.target.files[i];
-
-        var removeLink = '<div class="form-attachments__item" data-id="' + fileId + '">' +
-            '<div class="form-attachments__item-wrapper">' +
-            '<img class="form-attachments__item-image" src="' + window.URL.createObjectURL(this.files[fileId]) + '" alt="' + file.name + '">' +
-            '<div class="form-attachments__item-name">' + file.name + '</div>' +
-            '<div class="form-attachments__item-size">' + (file.size / 1024).toFixed(1) + 'Кб' + '</div>' +
-            '<div class="form-attachments__item-link">×</div>' +
-            '</div>' +
-            '</div>';
-        $('.form-attachments__items').append(removeLink);
-    }
-});
-
-  $(document).on("click",".form-attachments__item-link", function (e) {
-    $(this).closest('.form-attachments__item').remove();
-  });
-
   $(document).on("click",".albums", function (e) {
     $(".showAlbums").toggle("easing");
   });
@@ -38,5 +9,5 @@ $(document).on('change', '.attachments', function (e) {
     $(".info input").select();
     document.execCommand("copy");
   });
-  
+
   });
