d744dff455373dc290f2cc7768ba617872f41f84
Ostap <semenchuk.ostap@icloud.com>
2021-11-16 20:27:12 +0200

    Remove test for welcome page

--- spec/helpers/welcome_pages_helper_spec.rb
+++ spec/helpers/welcome_pages_helper_spec.rb
@@ -2,11 +2,3 @@ require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
 # the WelcomePagesHelper. For example:
-#
-# describe WelcomePagesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
