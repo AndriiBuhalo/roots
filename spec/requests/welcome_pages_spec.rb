ef8be833ff1e1218a88eef8146278f6f2350dfed
SvitDZHUS <svitdzhus@gmail.com>
2021-11-17 19:34:31 +0200

    Tramsformed all view to use more bootstrap

--- spec/requests/welcome_pages_spec.rb
+++ spec/requests/welcome_pages_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "WelcomePages", type: :request do
     it "returns http success" do
       get "/welcome_pages/home"
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:home, layout: :application)
     end
   end
 
@@ -12,6 +13,7 @@ RSpec.describe "WelcomePages", type: :request do
     it "returns http success" do
       get "/welcome_pages/about"
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:about, layout: :application)
     end
   end
 
@@ -19,6 +21,7 @@ RSpec.describe "WelcomePages", type: :request do
     it "returns http success" do
       get "/welcome_pages/contacts"
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:contacts, layout: :application)
     end
   end
 
--- spec/views/welcome_pages/contacts.html.slim_spec.rb
+++ spec/views/welcome_pages/contacts.html.slim_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "welcome_pages/contacts.html.slim", type: :view do
+  include Devise::Test::ControllerHelpers
+
+	before(:each) do
+	  render :template => "welcome_pages/contacts", :layout => "layouts/application"
+	end
+
+	it "has header menu for public" do
+	  expect(rendered).to have_title t('global.page_title')
+    expect(rendered).to have_link t('global.header.home'), href: welcome_pages_home_path
+    expect(rendered).to have_link t('global.header.sign_up'), href: new_user_registration_path
+    expect(rendered).to have_link t('global.header.log_in'), href: new_user_session_path
+	end
+
+	it "has _contacts partial" do
+    expect(rendered).to include t('welcome_pages.contacts.title')
+    expect(rendered).to include t('welcome_pages.contacts.subtitle')
+  end
+
+  it "has footer navbar" do
+    expect(rendered).to include t('global.footer.string')
+    expect(rendered).to have_link t('global.footer.about'), href: welcome_pages_about_path
+    expect(rendered).to have_link t('global.footer.contacts'), href: welcome_pages_contacts_path
+  end
+
 end
