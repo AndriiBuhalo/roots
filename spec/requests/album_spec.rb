216c2b40de451d8d6ff1e87d9917c18f7ed3a2c1
Valik <84660000+N1KeLoS@users.noreply.github.com>
2021-11-17 20:05:53 +0200

    Update spec/requests/album_spec.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- spec/requests/album_spec.rb
+++ spec/requests/album_spec.rb
@@ -1,7 +1,7 @@
  require 'rails_helper'
 
  RSpec.describe "/album", type: :request do
-  describe "GET /index" do
+  describe 'GET /index' do
     it 'renders a successful response' do
       get album_url
       expect(response).to be_successful
