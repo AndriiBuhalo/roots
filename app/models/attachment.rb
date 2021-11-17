f1ddb6c1c2b43a72b04061aa0cb8cff0ffacb7ea
Valik <84660000+N1KeLoS@users.noreply.github.com>
2021-11-17 19:51:59 +0200

    Update app/models/attachment.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 class Attachment < ApplicationRecord
   has_many :attachment_relations
   has_many :albums, through: :attachment_relations, source: :attachable,
-           source_type: 'Album', dependent: :destroy
+                    source_type: 'Album', dependent: :destroy
   mount_uploader :file, FileUploader
 end
