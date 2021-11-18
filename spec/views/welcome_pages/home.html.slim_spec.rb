b8882da00edabdec476a5404688be1c83cae6aae
SvitDZHUS <svitdzhus@gmail.com>
2021-11-18 10:36:45 +0200

    Cleaned code after rubocop review

--- spec/views/welcome_pages/home.html.slim_spec.rb
+++ spec/views/welcome_pages/home.html.slim_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "welcome_pages/home.html.slim", type: :view do
+RSpec.describe 'welcome_pages/home.html.slim', type: :view do
   include Devise::Test::ControllerHelpers
 
 	before(:each) do
-	  render :template => "welcome_pages/home", :layout => "layouts/application"
+	  render template: 'welcome_pages/home', layout: 'layouts/application'
 	end
 
-	it "has header menu for public" do
+	it 'has header menu for public' do
 	  expect(rendered).to have_title t('global.page_title')
     expect(rendered).to have_link t('global.header.home'), href: welcome_pages_home_path
     expect(rendered).to have_link t('global.header.sign_up'), href: new_user_registration_path
     expect(rendered).to have_link t('global.header.log_in'), href: new_user_session_path
 	end
 
-	it "has _home partial" do
+	it 'has _home partial' do
     expect(rendered).to include t('welcome_pages.home.title')
     expect(rendered).to include t('welcome_pages.home.subtitle')
   end
 
-  it "has footer navbar" do
+  it 'has footer navbar' do
     expect(rendered).to include t('global.footer.string')
     expect(rendered).to have_link t('global.footer.about'), href: welcome_pages_about_path
     expect(rendered).to have_link t('global.footer.contacts'), href: welcome_pages_contacts_path
