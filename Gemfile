610fe0684626984d071763095fc39a61916ddc3b
SvitDZHUS <svitdzhus@gmail.com>
2021-11-24 11:25:23 +0200

    Tests were modified

--- Gemfile
+++ Gemfile
@@ -5,22 +5,22 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.4'
 
-gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
-gem 'pg', '~> 1.1'
-gem 'puma', '~> 5.0'
-gem 'sass-rails', '>= 6'
-gem 'webpacker', '~> 5.0'
-gem 'turbolinks', '~> 5'
-gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'devise'
 gem 'devise-i18n'
 gem 'dotenv-rails', '~> 2.7'
+gem 'jbuilder', '~> 2.7'
 gem 'omniauth-google-oauth2'
 gem 'omniauth-rails_csrf_protection'
+gem 'pg', '~> 1.1'
+gem 'puma', '~> 5.0'
+gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 gem 'rails-i18n', '~> 6.0.0'
+gem 'sass-rails', '>= 6'
 gem 'simple_form'
 gem 'slim-rails'
+gem 'turbolinks', '~> 5'
+gem 'webpacker', '~> 5.0'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
@@ -33,7 +33,6 @@ group :development, :test do
 end
 
 group :development do
-  gem 'web-console', '>= 4.1.0'
   gem 'listen', '~> 3.3'
   gem 'rack-mini-profiler', '~> 2.0'
   gem 'rubocop', '~> 1.22', require: false
@@ -41,6 +40,7 @@ group :development do
   gem 'rubocop-rails', '~> 2.12', require: false
   gem 'rubocop-rspec', '~>2.5.0', require: false
   gem 'spring'
+  gem 'web-console', '>= 4.1.0'
 end
 
 group :test do
--- spec/views/welcome_pages/home.html.slim_spec.rb
+++ spec/views/welcome_pages/home.html.slim_spec.rb
@@ -5,43 +5,29 @@ require 'rails_helper'
 RSpec.describe 'welcome_pages/home.html.slim', type: :view do
   include Devise::Test::ControllerHelpers
 
-  describe 'home page for public' do
-    it 'has header menu for public' do
-      render template: 'welcome_pages/home', layout: 'layouts/public'
-      expect(rendered).to have_title t('layouts.page_title')
-      expect(rendered).to have_link t('layouts.header.sign_up'), href: new_user_registration_path
-      expect(rendered).to have_link t('layouts.header.log_in'), href: new_user_session_path
-    end
-  end
-
-  describe 'home page for user' do
-    it 'has header menu for user' do
-      user = FactoryBot.create(:user)
-      sign_in user
-      render template: 'welcome_pages/home', layout: 'layouts/application'
-      expect(rendered).to have_title t('layouts.page_title')
-      expect(rendered).to have_link t('layouts.header.posts'), href: posts_path
-      expect(rendered).to have_link t('layouts.header.albums')
-      expect(rendered).to have_css('img.gravatar')
-      sign_out user
-    end
-  end
-
-  describe 'home page and footer' do
-    before do
-      render template: 'welcome_pages/home', layout: 'layouts/public'
-    end
-
+  context 'user is not logged in' do
     it 'has _home partial' do
+      render
       expect(rendered).to include t('welcome_pages.home.title')
       expect(rendered).to include t('welcome_pages.home.subtitle')
+      expect(rendered).to have_link t('welcome_pages.home.sign_up_link'), href: new_user_registration_path
+    end
+  end
+
+  context 'user is logged in' do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      sign_in user
+      render
     end
 
-    it 'has footer navbar' do
-      expect(rendered).to include t('layouts.footer.string')
-      expect(rendered).to have_link t('layouts.footer.about'), href: welcome_pages_about_path
-      expect(rendered).to have_link t('layouts.footer.contacts'), href: welcome_pages_contacts_path
-      expect(rendered).to have_link t('layouts.footer.softserve'), href: 'https://www.softserveinc.com/uk-ua'
+    after do
+      sign_out user
+    end
+
+    it 'has not a Sign Up link for user' do
+      expect(rendered).not_to have_link t('welcome_pages.home.sign_up_link'), href: new_user_registration_path
     end
   end
 end
