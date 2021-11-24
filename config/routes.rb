6e35919e35e9fe5de5609dc56339ea50e077e384
SvitDZHUS <svitdzhus@gmail.com>
2021-11-24 13:35:51 +0200

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :locales, only: :update, constraints: { id: /(en|uk)/ }
+  scope "(:locale)", locale: /en|uk/ do
     resources :posts
     get 'welcome_pages/home'
     get 'welcome_pages/about'
     get 'welcome_pages/contacts'
-    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    devise_for :users, skip: :omniauth_callbacks
     devise_scope :user do
       authenticated :user do
         root 'posts#index', as: :authenticated_root
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
       end
       get '/users/sign_out' => 'devise/sessions#destroy'
     end
+  end
+  devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
 end
