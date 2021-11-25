d2d67f2c77362094aaa47114e58dc632264489d8
Max Stankovskyi <stankovskijm@gmail.com>
2021-11-25 13:39:42 +0200

    association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   validates :title, presence: true, length: { minimum: 3, maximum: 20 }
   validates :content, presence: true, length: { minimum: 5, maximum: 100 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,8 @@ ActiveRecord::Schema.define(version: 2021_11_22_124718) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
