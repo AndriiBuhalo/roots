5bc72b84cf780f231120d2d1c3493d916a3377ca
Nikolay010 <timofeev7300@ukr.net>
2021-11-25 13:21:41 +0200

    application

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-#Dotenv::Railtie.load
+Dotenv::Railtie.load
 
 module Roots
   class Application < Rails::Application
