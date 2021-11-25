4af8cf72b08b7f47ad1287f301a31ab4f46b3bc9
Max Stankovskyi <stankovskijm@gmail.com>
2021-11-25 20:44:16 +0200

    added concerns

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  include Authorable
   validates :title, presence: true, length: { minimum: 3, maximum: 20 }
   validates :content, presence: true, length: { minimum: 5, maximum: 100 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_124718) do
+ActiveRecord::Schema.define(version: 2021_11_25_174304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 2021_11_22_124718) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
