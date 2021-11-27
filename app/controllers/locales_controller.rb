de93f860287c6f606fbf83721b6406f36af5b5c9
SvitDZHUS <svitdzhus@gmail.com>
2021-11-27 19:52:59 +0200

    Locale is stored in cookies now

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,7 +1,6 @@
 class LocalesController < ApplicationController
   def update
-    session[:locale] = params[:id]
-    # cookies.permanent[:locale] = params[:id]
+    cookies.permanent[:locale] = params[:id]
     redirect_to request.referer, notice: t('global.translate')
   end
 end
