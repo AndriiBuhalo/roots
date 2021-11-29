6e7bad0d5367cf443131d25d28a465e0f9f5c948
= <fergo.rossii@gmail.com>
2021-11-29 13:34:28 +0200

    fix rubocop

--- db/migrate/20211117165612_remove_column_from_attachments.rb
+++ db/migrate/20211117165612_remove_column_from_attachments.rb
@@ -5,5 +5,6 @@ class RemoveColumnFromAttachments < ActiveRecord::Migration[6.1]
     remove_column :attachments, :title
     remove_column :attachments, :created_at
     remove_column :attachments, :updated_at
+    remove_column :attachments, :date_accuracy
   end
 end
--- db/migrate/20211129091857_change_columns_attachments.rb
+++ db/migrate/20211129091857_change_columns_attachments.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 class ChangeColumnsAttachments < ActiveRecord::Migration[6.1]
   def change
     rename_column :attachments, :keywords, :keyword
     rename_column :attachments, :file_date, :date
-    remove_column :attachments, :date_accuracy
   end
 end
