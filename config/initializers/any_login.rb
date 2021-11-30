a628495e0353a3485c79eb1709e9908175b4265a
Nikolay010 <timofeev7300@ukr.net>
2021-11-30 10:08:00 +0200

    use conf any_login

--- config/initializers/any_login.rb
+++ config/initializers/any_login.rb
@@ -4,7 +4,7 @@ AnyLogin.setup do |config|
   config.name_method = proc { |t| [t.email, t.id] }
 
   # #provider	(:devise, :authlogic, sorcery, clearance). Provider can be identified automatically based on your Gemfile
-  # config.provider = nil
+  config.provider = :devise
   # end
 
   # # enabled or not
@@ -20,16 +20,16 @@ AnyLogin.setup do |config|
   # config.name_method = proc { |e| [e.email, e.id] }
 
   # # after logging in redirect user to path
-  #  config.redirect_path_after_login = :root_path
+  config.redirect_path_after_login = :root_path
 
   # # login on select change event OR click on button, or BOTH
-  # config.login_on = :both
+  config.login_on = :both
 
   # # position of any_login box top_left, top_right, bottom_left, bottom_right
   # config.position = :bottom_left
 
   # # label on Login button
-  #  config.login_button_label = 'Log in'
+  #  config.login_button_label = 'Login'
 
   # # prompt message in select
   # config.select_prompt = "Select #{AnyLogin.klass_name}"
