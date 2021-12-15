ef803c6c7795529d202884e977b4b39741922833
= <fergo.rossii@gmail.com>
2021-12-15 20:23:03 +0200

    added police for attachment and album

---
+++ spec/policies/album_policy_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AlbumPolicy, type: :policy do
+  subject(:post_policy) { described_class }
+
+  let(:user) { create(:user) }
+  let(:post) { create(:post, created_by: user) }
+  let(:policy_scope) { PostPolicy::Scope.new(user, Album).resolve }
+
+  permissions '.scope' do
+    let(:user_posts) { user.posts << post }
+
+    it 'allows the user to see his posts' do
+      expect(policy_scope).to eq user_posts
+    end
+  end
+
+  permissions :show?, :create?, :new?, :update?, :edit?, :destroy? do
+    describe 'current user of post' do
+      it 'grants access if user is current user of post' do
+        expect(post_policy).to permit(user, post)
+      end
+    end
+
+    describe 'Admin' do
+      let(:admin) { create(:user, :admin) }
+      let(:post) { create(:post, created_by: admin) }
+
+      it 'grants access if user is an admin' do
+        expect(post_policy).to permit(admin, post)
+      end
+    end
+
+    describe 'not current_user of post' do
+      let(:not_current_user) { create(:user) }
+
+      it 'denies access if user is not current user of post' do
+        expect(post_policy).not_to permit(not_current_user, post)
+      end
+    end
+  end
+end
