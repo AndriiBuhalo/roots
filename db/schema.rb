dddf0bfc0563a6ef1d6f0e6aeb35ae8914d278f1
SvitDZHUS <svitdzhus@gmail.com>
2021-12-23 22:41:55 +0200

    Hotfix

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2021_12_18_201630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "contacts", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "important_dates", force: :cascade do |t|
     t.string "event_name"
     t.date "event_date"
